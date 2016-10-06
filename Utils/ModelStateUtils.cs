c3e0b1281270f4b909ce3573e56549e06ca51614
sobelito <sobelito@gmail.com>
2016-10-06 10:10:25 +0000

    new sets of model state utils for api and mvc

--- Utils/ModelStateUtils.cs
+++ Utils/ModelStateUtils.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
 
 namespace MoarUtils.Utils {
   public class ModelStateUtils {
-    public static List<string> GetErrorListFromModelState(
-      ModelStateDictionary modelState
+    public static List<string> GetErrorListFromModelStateMvc(
+      System.Web.Mvc.ModelStateDictionary modelState
+    ) {
+      var query = from state in modelState.Values
+                  from error in state.Errors
+                  select error.ErrorMessage;
+
+      var errorList = query.ToList();
+      return errorList;
+    }
+
+    public static List<string> GetErrorListFromModelStateWebApi(
+      System.Web.Http.ModelBinding.ModelStateDictionary modelState
     ) {
       var query = from state in modelState.Values
                   from error in state.Errors
