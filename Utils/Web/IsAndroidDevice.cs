c89649c92e22fd822873ba67a092c381f38771f1
sobelito <sobelito@gmail.com>
2017-02-22 02:45:05 -0800

    added android detection, and allow cross frame

---
+++ Utils/Web/IsAndroidDevice.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace MoarUtils.Utils.Web {
+  public class IsAndroidDevice {
+    public static bool Execute(HttpRequestBase hrb) {
+      var userAgent = hrb.UserAgent.ToLower();
+      return (
+        userAgent.Contains("android")
+      );
+    }
+  }
+}
+
+
---
+++ filters/XFrameOptionAllowAll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MoarUtils.filters {
+  public class XFrameOptionAllowAll : ActionFilterAttribute {
+    public override void OnActionExecuting(ActionExecutingContext filterContext) {
+      filterContext.HttpContext.Response.Headers.Remove("X-Frame-Options");
+      filterContext.HttpContext.Response.AddHeader("X-Frame-Options", "AllowAll");
+
+      base.OnActionExecuting(filterContext);
+    }
+  }
+}
+
+  
