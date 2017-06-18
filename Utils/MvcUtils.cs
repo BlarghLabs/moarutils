6aec4da34d6578189c6da5b1efc1b79a38d57adc
sobelito <sobelito@gmail.com>
2017-06-18 22:28:31 +0000

    action name fix for ajax removed hints

--- Utils/MvcUtils.cs
+++ Utils/MvcUtils.cs
@@ -4,18 +4,22 @@ namespace MoarUtils.Utils {
   public class MvcUtils {
     public static string controllerName {
       get {
-        var cn = HttpContext.Current.Request.RequestContext.RouteData.Values["Controller"].ToString().ToLower();
+        var cn = ((HttpContext.Current == null) || (HttpContext.Current.Request == null))
+          ? ""
+          : HttpContext.Current.Request.RequestContext.RouteData.Values["Controller"].ToString().ToLower();
         return cn;
       }
     }
     public static string actionName {
       get {
-        var an = HttpContext.Current.Request.RequestContext.RouteData.Values["Action"].ToString().ToLower();
+        var an = ((HttpContext.Current == null) || (HttpContext.Current.Request == null))
+          ? ""
+          : HttpContext.Current.Request.RequestContext.RouteData.Values["Action"].ToString().ToLower();
         return an;
       }
     }
 
-    public static bool isMatch(string controller, string action){
+    public static bool isMatch(string controller, string action) {
       return (controllerName == controller) && (actionName == action);
     }
   }
