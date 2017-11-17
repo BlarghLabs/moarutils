3a3add0b4b8dc0a0913f61c40c40fec210f4399a
Brian Sobel <sobelito@gmail.com>
2017-11-17 14:05:59 +0000

    nuget and dependency updates

--- Utils/Strings.cs
+++ Utils/Strings.cs
@@ -1,6 +1,7 @@
-﻿using RestSharp.Extensions.MonoHttp;
+﻿//using RestSharp.Extensions.MonoHttp;
 using System;
 using System.Text.RegularExpressions;
+using System.Web;
 
 namespace MoarUtils.Utils {
   public class Strings {
