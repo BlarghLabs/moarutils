73841d3da4f14674e559b4fcb800442a3a8264e5
Brian Sobel <sobelito@gmail.com>
2019-06-04 19:35:23 +0000

    split list

---
+++ commands/lists/SplitList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoarUtils.commands.lists {
+  public class SplitList {
+    public static IEnumerable<List<T>> Execute<T>(List<T> locations, int nSize = 30) {
+      for (int i = 0; i < locations.Count; i += nSize) {
+        yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
+      }
+    }
+  }
+}
