6e55cbe0a0740d43210cd60b30aba30b76a3a04d
Brian Sobel <sobelito@gmail.com>
2019-06-05 03:41:45 +0000

    sha1

--- commands/hashing/GetSha1.cs
+++ commands/hashing/GetSha1.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿using MoarUtils.Utils;
+using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace MoarUtils.Utils {
-  public class Hash {
-    public static string GetSha1(string path) {
+namespace MoarUtils.commands.hashing {
+  public class GetSha1 {
+    public static string ExecuteFile(string path) {
       try {
         using (FileStream fs = new FileStream(path, FileMode.Open)) {
-          return GetSha1(fs);
+          return Execute(fs);
         }
       } catch (Exception ex) {
         LogIt.E(ex);
         throw ex;
       }
     }
-    public static string GetSha1(Stream s) {
+
+    public static string Execute(Stream s) {
       try {
         using (var bs = new BufferedStream(s)) {
           using (var sha1 = new SHA1Managed()) {
@@ -34,7 +36,7 @@ namespace MoarUtils.Utils {
       }
     }
 
-    public static string GetSha1(byte[] ba) {
+    public static string Execute(byte[] ba) {
       try {
         using (var sha1 = new SHA1Managed()) {
           byte[] hash = sha1.ComputeHash(ba);
@@ -50,5 +52,23 @@ namespace MoarUtils.Utils {
         throw ex;
       }
     }
+
+
+    public static string Execute(string input) {
+      using (var sha1 = new SHA1Managed()) {
+        var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+        var sb = new StringBuilder(hash.Length * 2);
+
+        foreach (byte b in hash) {
+          // can be "x2" if you want lowercase
+          sb.Append(b.ToString("X2"));
+        }
+
+        return sb.ToString();
+      }
+    }
   }
 }
+
+//0C2E99D0949684278C30B9369B82638E1CEAD415
+//0123456789012345678901234567890123456789
