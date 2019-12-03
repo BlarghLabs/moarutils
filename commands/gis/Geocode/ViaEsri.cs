29a920edc728ca24d3822330ad22f785465ebbe7
Brian Sobel <sobelito@gmail.com>
2019-12-03 22:22:29 +0000

    added no alphha num check, fixed parsing error, nuget

--- commands/gis/Geocode/ViaEsri.cs
+++ commands/gis/Geocode/ViaEsri.cs
@@ -7,6 +7,7 @@ using RestSharp;
 using System;
 using System.Net;
 using System.Web;
+using System.Text.RegularExpressions;
 
 namespace moarutils.utils.gis.geocode {
   //http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text=1700 Penny ave washingtn dc&f=pjson&forStorage=false&maxLocations=1
@@ -32,6 +33,19 @@ namespace moarutils.utils.gis.geocode {
         //note: ersi puts "0" in china, maybe ignore this one input?
         if (address.Equals("0")) {
           status = $"0 is not properly geocoded by this provider";
+          hsc = HttpStatusCode.BadRequest;
+          return;
+        }
+
+        if (address.Length > 200) {
+          LogIt.W("address was > 200 length, truncating: " + address);
+          address = address.Substring(0, 200);
+        }
+
+        var rgx = new Regex(@"[^\w\s]*");
+        var addressCheck = rgx.Replace(address.Trim(), "");
+        if (string.IsNullOrWhiteSpace(addressCheck)) {
+          status = $"address had no numbers or letters";
           hsc = HttpStatusCode.BadRequest;
           return;
         }
@@ -66,21 +80,28 @@ namespace moarutils.utils.gis.geocode {
         var content = response.Content;
         dynamic json = JObject.Parse(content);
 
-        if (json.locations.Count > 0) {
-          var geometry = json.locations[0].feature.geometry;
-          if (geometry != null) {
-            var lng = Convert.ToDecimal(geometry.x.Value);
-            var lat = Convert.ToDecimal(geometry.y.Value);
-            if ((lat != 0) && (lng != 0)) {
-              c.lng = lng;
-              c.lat = lat;
-            }
+        if ((json.locations == null) || (json.locations.Count == 0)) {
+          //maybe look at: json.error.code.Value
+          LogIt.W(content);
+          status = $"location result was null";
+          hsc = HttpStatusCode.BadRequest;
+          return;
+        }
+
+
+        var geometry = json.locations[0].feature.geometry;
+        if (geometry != null) {
+          var lng = Convert.ToDecimal(geometry.x.Value);
+          var lat = Convert.ToDecimal(geometry.y.Value);
+          if ((lat != 0) && (lng != 0)) {
+            c.lng = lng;
+            c.lat = lat;
           }
-          var precision = json.locations[0].feature.attributes;
-          if (precision != null) {
-            var pc = precision.Score.Value;
-            c.precision = Convert.ToString(pc);
-          }
+        }
+        var precision = json.locations[0].feature.attributes;
+        if (precision != null) {
+          var pc = precision.Score.Value;
+          c.precision = Convert.ToString(pc);
         }
 
         hsc = HttpStatusCode.OK;
