1b89c35f7cd5d9d824bc003a51c689fc37e3a122
Brian Sobel <sobelito@gmail.com>
2020-05-02 03:40:31 -0700

    new command

---
+++ commands/web/GetRawUrl.cs
@@ -0,0 +1,41 @@
+﻿using MoarUtils.commands.logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoarUtils.commands.web {
+  public class GetRawUrl {
+    /// <summary>
+    /// account for possbility of ELB sheilding the public ip
+    /// </summary>
+    /// <returns></returns>
+    public static string Execute(
+      HttpContext hc = null,
+      bool log = false
+    ) {
+      var rawUrl = "";
+      try {
+        if ((hc == null) || (hc.Request == null)) {
+          hc = HttpContext.Current;
+        }
+        if ((hc == null) || (hc.Request == null)) {
+          return null;
+        }
+
+        rawUrl = hc.Request.RawUrl;
+        return rawUrl;
+      } catch (Exception ex) {
+        LogIt.E(ex);
+      } finally {
+        if (log) {
+          LogIt.D(JsonConvert.SerializeObject(new {
+            rawUrl,
+          }, Formatting.Indented));
+        }
+      }
+      return null;
+    }
+  }
+}
