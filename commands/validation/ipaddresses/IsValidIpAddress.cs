4a761c35675de3c817cdce5c5258b59d148624d9
Brian Sobel <sobelito@gmail.com>
2021-05-17 00:06:48 -0400

    ip validation

---
+++ commands/validation/ipaddresses/IsValidIpAddress.cs
@@ -0,0 +1,31 @@
+﻿using MoarUtils.commands.logging;
+using System;
+using System.Net;
+
+namespace MoarUtils.commands.validation.ipaddresses {
+  public class IsValidIpAddress {
+    public bool Execute(string input) {
+      try {
+        IPAddress address;
+        if (IPAddress.TryParse(input, out address)) {
+          switch (address.AddressFamily) {
+            case System.Net.Sockets.AddressFamily.InterNetwork:
+              // we have IPv4
+              return true;
+            case System.Net.Sockets.AddressFamily.InterNetworkV6:
+              // we have IPv6
+              return true;
+            default:
+              // umm... yeah... I'm going to need to take your red packet and...
+              return false;
+          }
+        }
+        return false;
+      } catch (Exception ex) {
+        LogIt.E(ex);
+        return false;
+      }
+
+    }
+  }
+}
---
+++ commands/validation/ipaddresses/IsValidIpV4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Text;
+using System.Linq;
+
+namespace MoarUtils.commands.validation.ipaddresses {
+  public class IsValidIpV4 {
+    public bool Execute(string ip) {
+      if (String.IsNullOrWhiteSpace(ip)) {
+        return false;
+      }
+
+      string[] splitValues = ip.Split('.');
+      if (splitValues.Length != 4) {
+        return false;
+      }
+
+      byte tempForParsing;
+
+      return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+    }
+  }
+}
