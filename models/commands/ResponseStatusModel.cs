e5abd2c1b103bc5cce96876016f3d95466968400
Brian Sobel <sobelito@gmail.com>
2025-01-20 13:25:00 -0500

    wip

---
+++ models/commands/ResponseStatusModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace MoarUtils.models.commands {
+  public class ResponseStatusModel {
+
+    public HttpStatusCode httpStatusCode { get; set; } = HttpStatusCode.BadRequest;
+    public string status { get; set; }
+  }
+}
